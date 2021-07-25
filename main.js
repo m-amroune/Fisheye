7b7d398b36a924b9487e4c95764dda6e8bc725d1
m-amroune <amroune.moustapha@gmail.com>
2021-07-25 14:22:37 +0200

    integration main page with fetch and async wait

--- main.js
+++ main.js
@@ -1,45 +1,40 @@
 const section = document.getElementById("photographersGallery");
 
-// loading jsonfile in variable
-let requestURL =
-  "https://github.com/m-amroune/MoustaphaAmroune_6_10072021/blob/main/data.json";
-// new object XMLHttpRequest
-let request = new XMLHttpRequest();
+let photographersData = [];
 
-// Use method GET for recuperate json data
-request.open("GET", requestURL);
+const fetchPhotographers = async () => {
+  await fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => (photographersData = data.photographers));
 
-request.responseType = "json"; // wait response in json format
-request.send();
-
-request.onload = function () {
-  let photographer = request.responseType;
-  showPhotographer(photographer);
+  console.log(photographersData);
 };
 
-function showPhotographer(jsonObj) {
-  let photographer = jsonObj["photographers"];
+const photographersDisplay = async () => {
+  await fetchPhotographers();
+
+  let photographer = photographersData;
 
   for (let i = 0; i < photographer.length; i++) {
     let ListArticle = document.createElement("article");
-    let imgPhotographer = document("img");
+    let imgPhotographer = document.createElement("img");
     let cardH2 = document.createElement("h2");
     let detailsPara1 = document.createElement("p");
     let detailsPara2 = document.createElement("p");
     let detailsPara3 = document.createElement("p");
     let tagsList = document.createElement("ul");
 
-    imgPhotographer.textContent = photographer[i].portrait;
+    imgPhotographer.src = photographer[i].portrait;
     cardH2.textContent = photographer[i].name + photographer[i].portrait;
     detailsPara1.textContent =
-      photographer[i].city + ", " + photographer.country;
+      photographer[i].city + ", " + photographer[i].country;
     detailsPara2.textContent = photographer[i].tagline;
-    detailsPara3.textContent = photographer[i].price;
+    detailsPara3.textContent = photographer[i].price + "€/jour";
 
     let listTags = photographer[i].tags;
     for (let j = 0; j < listTags.length; j++) {
       let listItem = document.createElement("li");
-      listItem.textContent = listItem[j];
+      listItem.textContent = "#" + listTags[j];
       tagsList.appendChild(listItem);
     }
 
@@ -48,7 +43,9 @@ function showPhotographer(jsonObj) {
     ListArticle.appendChild(detailsPara1);
     ListArticle.appendChild(detailsPara2);
     ListArticle.appendChild(detailsPara3);
-    ListArticle.appendChild(tagsList);
-    section.appendChild();
+    ListArticle.appendChild(tagsList).style.display = "flex";
+    section.appendChild(ListArticle);
   }
-}
+};
+
+photographersDisplay();
