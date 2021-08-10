adca4409c49e6256f88d3e0307300992a4d7e673
m-amroune <amroune.moustapha@gmail.com>
2021-08-10 15:40:32 +0200

    display cards and tags for main page with JS + stylization

--- main.js
+++ main.js
@@ -1,51 +1,89 @@
 const section = document.getElementById("photographersGallery");
+const header = document.querySelector(".header");
 
 let photographersData = [];
+let headerTags = [];
 
 const fetchPhotographers = async () => {
   await fetch("data.json")
     .then((res) => res.json())
     .then((data) => (photographersData = data.photographers));
-
-  console.log(photographersData);
 };
 
 const photographersDisplay = async () => {
   await fetchPhotographers();
 
-  let photographer = photographersData;
+  // creation elements tags navigation
+  const navigation = document.createElement("nav");
+  const tagsListNavigation = document.createElement("ul");
 
-  for (let i = 0; i < photographer.length; i++) {
-    let ListArticle = document.createElement("article");
-    let imgPhotographer = document.createElement("img");
-    let cardH2 = document.createElement("h2");
-    let detailsPara1 = document.createElement("p");
-    let detailsPara2 = document.createElement("p");
-    let detailsPara3 = document.createElement("p");
-    let tagsList = document.createElement("ul");
+  // creation class tags navigation
+  tagsListNavigation.classList.add("tagListNavigation");
+  navigation.classList.add("navigation");
 
-    imgPhotographer.src = photographer[i].portrait;
-    cardH2.textContent = photographer[i].name + photographer[i].portrait;
+  let headerTags = photographersData;
+  const listHashtags = headerTags.map((headerTags) => headerTags.tags); // conversion in array
+  console.log(listHashtags);
+  listHashtagsSingle = [].concat(...listHashtags); // single array
+  console.log(listHashtagsSingle);
+  let HashtagsWithoutDuplicate = Array.from(new Set(listHashtagsSingle)); // delete duplication
+  console.log(HashtagsWithoutDuplicate);
+
+  for (let j = 0; j < HashtagsWithoutDuplicate.length; j++) {
+    let listItem = document.createElement("li");
+    listItem.textContent = listItem[j];
+    listItem.textContent = "#" + HashtagsWithoutDuplicate[j];
+    tagsListNavigation.appendChild(listItem);
+  }
+
+  // appendChild in nav
+  navigation.appendChild(tagsListNavigation);
+  header.appendChild(navigation);
+
+  for (let i = 0; i < photographersData.length; i++) {
+    // creation elements main
+    const listArticle = document.createElement("article");
+    const imgPhotographer = document.createElement("img");
+    const cardH2 = document.createElement("h2");
+    const detailsPara1 = document.createElement("p");
+    const detailsPara2 = document.createElement("p");
+    const detailsPara3 = document.createElement("p");
+    const tagsList = document.createElement("ul");
+
+    imgPhotographer.classList.add("imgPhotographer");
+    cardH2.classList.add("namePhotographer");
+    detailsPara1.classList.add("localisationPhotographer");
+    detailsPara2.classList.add("citationPhotographer");
+    detailsPara3.classList.add("pricePhotographer");
+    tagsList.classList.add("tagsList");
+    listArticle.classList.add("listArticle");
+
+    // integration html main
+    imgPhotographer.src =
+      "./public/img/Photographers ID Photos/" + photographersData[i].portrait;
+    cardH2.textContent = photographersData[i].name;
     detailsPara1.textContent =
-      photographer[i].city + ", " + photographer[i].country;
-    detailsPara2.textContent = photographer[i].tagline;
-    detailsPara3.textContent = photographer[i].price + "€/jour";
+      photographersData[i].city + " " + photographersData[i].country;
+    detailsPara2.textContent = photographersData[i].tagline;
+    detailsPara3.textContent = photographersData[i].price + "€/jour";
 
-    let listTags = photographer[i].tags;
+    let listTags = photographersData[i].tags;
     for (let j = 0; j < listTags.length; j++) {
       let listItem = document.createElement("li");
+      listItem.textContent = listItem[j];
       listItem.textContent = "#" + listTags[j];
       tagsList.appendChild(listItem);
     }
 
-    ListArticle.appendChild(imgPhotographer);
-    ListArticle.appendChild(cardH2);
-    ListArticle.appendChild(detailsPara1);
-    ListArticle.appendChild(detailsPara2);
-    ListArticle.appendChild(detailsPara3);
-    ListArticle.appendChild(tagsList).style.display = "flex";
-    section.appendChild(ListArticle);
+    // appendChild in article
+
+    listArticle.appendChild(imgPhotographer);
+    listArticle.appendChild(cardH2);
+    listArticle.appendChild(detailsPara1);
+    listArticle.appendChild(detailsPara2);
+    listArticle.appendChild(detailsPara3);
+    listArticle.appendChild(tagsList);
+    section.appendChild(listArticle);
   }
 };
-
 photographersDisplay();
