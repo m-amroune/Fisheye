c04b50b335a197dda633a430487a9da16cdb2062
m-amroune <amroune.moustapha@gmail.com>
2021-12-07 21:43:11 +0100

    suppr directory JS

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-import { HomePage } from "./homepage.js";
-
-async function createHomePage() {
-  let homePage = new HomePage();
-  await homePage.fetchPhotographers();
-  homePage.displayPhotographers();
-  homePage.displayTags();
-}
-createHomePage();
